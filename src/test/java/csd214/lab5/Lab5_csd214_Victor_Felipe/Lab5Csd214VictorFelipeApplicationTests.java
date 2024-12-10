5d69fa53e5669d0917655160c4507dcced73121c
Victor Felipe Jr <144490375+Dotsz-Vic-FLP@users.noreply.github.com>
2024-12-10 16:12:20 -0500

    separate repo for lab5 csd214

---
+++ src/test/java/csd214/lab5/Lab5_csd214_Victor_Felipe/Lab5Csd214VictorFelipeApplicationTests.java
@@ -0,0 +1,13 @@
+package csd214.lab5.Lab5_csd214_Victor_Felipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab5Csd214VictorFelipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
